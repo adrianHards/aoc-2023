0b98b14d410ed04c827b10343c35047b18e6a49d
Adrian Hards <github@ahards.com>
2023-12-02 13:05:54 +0000

    add initial solution

--- day_2/Program.cs
+++ day_2/Program.cs
@@ -11,9 +11,11 @@
 
             foreach (string line in lines)
             {
-                if (ValidGame(line))
+                int colonIndex = line.IndexOf(':');
+                string gameString = line[(colonIndex + 1)..];
+                if (ValidGame(gameString))
                 {
-                    sum += GameID(line);
+                    sum += GameID(line, colonIndex);
                 }
             }
             Console.WriteLine(sum);
@@ -24,16 +26,33 @@
         }
     }
 
-    static int GameID(string line)
+    static int GameID(string line, int colonIndex)
     {
-        int colonIndex = line.IndexOf(':');
         string game = line.Substring(0, colonIndex);
         int whiteSpaceIndex = game.IndexOf(' ');
         string gameID = game[whiteSpaceIndex..];
         return int.Parse(gameID);
     }
 
-    static bool ValidGame(string line)
+    static Dictionary<string, int> RGB = new()
     {
+        {"red", 12},
+        {"green", 13},
+        {"blue", 14}
+    };
+
+    static bool ValidGame(string gameString)
+    {
+        string[] gameValues = gameString.Split(new char[] { ',', ';' });
+        foreach (var value in gameValues)
+        {
+            string colour = value.Split(' ').Last();
+            int colourValue = int.Parse(value.TrimStart().Split(' ').First());
+            if (RGB[colour] < colourValue)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
