0bf4448fc6870f7aac84e3a77e7a0909ddbc3649
Adrian Hards <github@ahards.com>
2023-12-05 10:13:36 +0000

    add day 5

--- day_5/Program.cs
+++ day_5/Program.cs
@@ -15,17 +15,25 @@ class Program
             string seedString = sections[0].Split(":")[1].Trim();
             int[] seedsArray = Array.ConvertAll(seedString.Split(' '), int.Parse);
 
-            string seedToSoilString = sections[1].Split(':')[1].Trim();
-            int[][] seedToSoilMap = seedToSoilString.Split('\n')
-                .Select(line => line.Split(' ')
-                .Select(int.Parse).ToArray())
-                .ToArray();
-
+            int[][] seedToSoilMap = GetMap(sections[1]);
+            int[][] soilToFertMap = GetMap(sections[2]);
+            int[][] fertToWaterMap = GetMap(sections[3]);
+            int[][] waterToLightMap = GetMap(sections[4]);
+            int[][] lightToTempMap = GetMap(sections[5]);
+            int[][] tempToHumidMap = GetMap(sections[6]);
+            int[][] humidToLocationMap = GetMap(sections[7]);
         }
         catch (IOException e)
         {
             Console.WriteLine("An error occurred while reading the file: " + e.Message);
         }
+    }
+    static int[][] GetMap(string mapString)
+    {
+        return mapString.Split(':')[1].Trim().Split('\n')
+            .Select(line => line.Split(' ')
+            .Select(int.Parse).ToArray())
+            .ToArray();
     }
 }
 
