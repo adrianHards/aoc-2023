b2d4173a16841e8ab5c9694b8a98a0aa4444bbab
Adrian Hards <github@ahards.com>
2023-12-05 09:32:19 +0000

    add day 5

---
+++ day_5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day_5;
+
+class Program
+{
+    static async Task Main()
+    {
+        // string filePath = "./input.txt";
+        string filePath = "./testData.txt";
+
+        try
+        {
+            string seedContent = File.ReadAllText(filePath);
+
+            {
+                Console.WriteLine(seedContent);
+            }
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("An error occurred while reading the file: " + e.Message);
+        }
+    }
+}
+
