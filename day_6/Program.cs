402184be343fe51a38982c9d3abe9c30764d7b17
Adrian Hards <github@ahards.com>
2023-12-06 08:21:40 +0000

    prase data

---
+++ day_6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace day_6
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "./test.txt";
+            string raceData = File.ReadAllText(filePath);
+            string[] raceSections = raceData.Split("\n");
+
+            string timeValues = raceSections[0].Split(":")[1];
+            int[] timeArray = Array.ConvertAll(timeValues.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+            string distValues = raceSections[1].Split(":")[1];
+            int[] distArray = Array.ConvertAll(distValues.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+
+
+        }
+    }
+}
