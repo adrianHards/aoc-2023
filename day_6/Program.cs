71876a0da88abd90f8f72da3f71b44dda1d0adb1
Adrian Hards <github@ahards.com>
2023-12-06 08:50:46 +0000

    solution

--- day_6/Program.cs
+++ day_6/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main()
         {
-            string filePath = "./test.txt";
+            string filePath = "./input.txt";
             string raceData = File.ReadAllText(filePath);
             string[] raceSections = raceData.Split("\n");
 
@@ -14,7 +14,28 @@
             string distValues = raceSections[1].Split(":")[1];
             int[] distArray = Array.ConvertAll(distValues.Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
 
+            List<int> noOfWinsList = new List<int>();
 
+            for (int i = 0; i < timeArray.Length; i++)
+            {
+                int wins = 0;
+                int distToBeat = distArray[i];
+                int start = (int)Math.Ceiling((double)distArray[i] / timeArray[i]);
+                int end = timeArray[i] - 1;
+
+                for (int timeHeld = start; timeHeld <= end; timeHeld++)
+                {
+                    int timeRemain = timeArray[i] - timeHeld;
+                    if (timeHeld * timeRemain > distToBeat)
+                    {
+                        wins++;
+                    }
+                }
+                noOfWinsList.Add(wins);
+            }
+
+            int productOfWins = noOfWinsList.Aggregate(1, (acc, x) => acc * x);
+            Console.WriteLine($"wins: {productOfWins}");
         }
     }
 }
