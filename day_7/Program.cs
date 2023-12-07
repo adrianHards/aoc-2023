1ce29acbb0aa2bfe82420e356dc6bb52a93f21fb
Adrian Hards <github@ahards.com>
2023-12-07 09:36:55 +0000

    day 7 parse

--- day_7/Program.cs
+++ day_7/Program.cs
@@ -21,25 +21,50 @@
                 ["2"] = 2
             };
 
+            Dictionary<string, int> typeValue = new Dictionary<string, int>
+            {
+                { "5,0,0,0,0", 7 },
+                { "4,1,0,0,0", 6 },
+                { "3,2,0,0,0", 5 },
+                { "3,1,1,0,0", 4 },
+                { "2,2,1,0,0", 3 },
+                { "2,1,1,1,0", 2 },
+                { "1,1,1,1,1", 1 },
+            };
+
             {
                 string filePath = "./testinput.txt";
                 string[] rows = File.ReadAllLines(filePath);
 
-                Dictionary<string, (int multiplier, int count, int rank)> cardDictionary = new();
+                Dictionary<string, (int labelsAsInts, int multiplier, int type, int rank)> cardDictionary = new();
 
                 foreach (string row in rows)
                 {
                     string card = row.Substring(0, 5);
                     int multiplier = int.Parse(row.Substring(5).Trim());
-                    cardDictionary[card] = (multiplier, 0, 0);
+                    string labelsAsString = "";
+
+                    foreach (char c in card)
+                    {
+                        string label = c.ToString();
+                        if (cardValue.TryGetValue(label, out int value))
+                        {
+                            labelsAsString += value.ToString();
+                        }
+                    }
+
+                    int labelsAsInts = int.Parse(labelsAsString);
+                    cardDictionary[card] = (labelsAsInts, multiplier, 0, 0);
                 }
 
                 foreach (var kvp in cardDictionary)
                 {
                     string card = kvp.Key;
+                    Dictionary<char, int> labelCount = new();
+
                     foreach (char label in card)
                     {
-                        Dictionary<char, int> labelCount = new();
+
                         if (labelCount.ContainsKey(label))
                         {
                             labelCount[label]++;
@@ -49,6 +74,20 @@
                             labelCount[label] = 1;
                         }
                     }
+                    var orderedValues = labelCount.OrderByDescending(label => label.Value).Select(label => label.Value).ToArray();
+                    int[] resultArray = new int[5];
+                    Array.Copy(orderedValues, resultArray, Math.Min(5, orderedValues.Length));
+
+                    string resultKey = string.Join(",", resultArray);
+                    int typeRank = typeValue[resultKey];
+
+                    cardDictionary[card] = (cardDictionary[card].labelsAsInts, cardDictionary[card].multiplier, typeRank, cardDictionary[card].rank);
+                }
+
+                foreach (var kvp in cardDictionary)
+                {
+                    var (labelsAsInts, multiplier, type, rank) = kvp.Value;
+                    Console.WriteLine($"Card: {kvp.Key}, Num: {labelsAsInts}, Multiplier: {multiplier}, Type: {type}, Rank: {rank}");
                 }
             }
         }
