93b4408eea5ea04de3d617ed902b19b67f2ae10a
Adrian Hards <github@ahards.com>
2023-12-09 09:04:06 +0000

    day 9

--- day_9/Program.cs
+++ day_9/Program.cs
@@ -10,18 +10,35 @@ class Program
 
         for (int i = 0; i < rows.Length; i++)
         {
-            string[] values = rows[i].Split(' '); // Assuming values are separated by spaces
+            string[] values = rows[i].Split(' ');
             arrayOfIntegers[i] = Array.ConvertAll(values, int.Parse);
         }
 
-        foreach (int[] array in arrayOfIntegers)
+        foreach (int[] integersArray in arrayOfIntegers)
         {
+            var numbers = new List<int>(integersArray);
+            var histories = new List<List<int>>();
 
+            while (true)
+            {
+                var differences = new List<int>();
+
+                for (int i = 0; i < numbers.Count - 1; i++)
+                {
+                    differences.Add(numbers[i + 1] - numbers[i]);
+                }
+
+                histories.Add(differences);
+
+                if (differences.TrueForAll(d => d == 0)) break;
+
+                numbers = differences;
+            }
+
+            foreach (var history in histories)
+            {
+                Console.WriteLine(string.Join(" ", history));
+            }
         }
-
-        // foreach (int[] array in arrayOfIntegers)
-        // {
-        //     Console.WriteLine(string.Join(" ", array));
-        // }
     }
 }
