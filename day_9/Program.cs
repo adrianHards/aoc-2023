e6c196b5c7f771f7366ed908c57af9e6aa0e723b
Adrian Hards <github@ahards.com>
2023-12-09 07:17:24 +0000

    day 9

---
+++ day_9/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace day_9;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./testinput.txt";
+        string[] rows = File.ReadAllLines(filePath);
+        int[][] arrayOfIntegers = new int[rows.Length][];
+
+        for (int i = 0; i < rows.Length; i++)
+        {
+            string[] values = rows[i].Split(' '); // Assuming values are separated by spaces
+            arrayOfIntegers[i] = Array.ConvertAll(values, int.Parse);
+        }
+
+        foreach (int[] array in arrayOfIntegers)
+        {
+
+        }
+
+        // foreach (int[] array in arrayOfIntegers)
+        // {
+        //     Console.WriteLine(string.Join(" ", array));
+        // }
+    }
+}
