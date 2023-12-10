bf564ce935150af13fa49d358d056b973dfd1769
Adrian Hards <github@ahards.com>
2023-12-10 08:04:50 +0000

    day 10

---
+++ day_10/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace day_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./testinput.txt";
+        string[] rows = File.ReadAllLines(filePath);
+
+        char[][] arraysOfChars = new char[rows.Length][];
+        for (int i = 0; i < rows.Length; i++)
+        {
+            arraysOfChars[i] = rows[i].ToCharArray();
+        }
+
+        int rowIndexS = 0;
+        int columnIndexS = 0;
+
+        for (int i = 0; i < arraysOfChars.Length; i++)
+        {
+            for (int j = 0; j < arraysOfChars[i].Length; j++)
+            {
+                if (arraysOfChars[i][j] == 'S')
+                {
+                    rowIndex = i;
+                    columnIndex = j;
+                    break;
+                }
+            }
+        }
+    }
+}
