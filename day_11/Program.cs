00c64b3ce06a052a3df4b0a51467b22a2d8620fe
Adrian Hards <github@ahards.com>
2023-12-12 07:43:59 +0000

    day 11

---
+++ day_11/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace day_11;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "./testinput.txt";
+        string[] rows = File.ReadAllLines(filePath);
+        string[][] arrayOfStars = new string[rows.Length][];
+    }
+
+    IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> elements, int k)
+    {
+        int i = 0;
+        foreach (T element in elements)
+        {
+            foreach (var combination in Combinations(elements.Skip(i + 1), k - 1))
+            {
+                yield return new T[] { element }.Concat(combination);
+            }
+            i++;
+        }
+    }
+}
+
