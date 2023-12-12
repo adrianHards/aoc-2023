19e11b5e37bfc4f62c55cc2d9e39ec0500cff894
Adrian Hards <github@ahards.com>
2023-12-12 08:25:01 +0000

    day 11

--- day_11/Program.cs
+++ day_11/Program.cs
@@ -1,25 +1,46 @@
-﻿namespace day_11;
-
-class Program
+﻿namespace day_11
 {
-    static void Main(string[] args)
+    class Program
     {
-        string filePath = "./testinput.txt";
-        string[] rows = File.ReadAllLines(filePath);
-        string[][] arrayOfStars = new string[rows.Length][];
-    }
-
-    IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> elements, int k)
-    {
-        int i = 0;
-        foreach (T element in elements)
+        static void Main(string[] args)
         {
-            foreach (var combination in Combinations(elements.Skip(i + 1), k - 1))
+            string filePath = "./testinput.txt";
+            string[] rows = File.ReadAllLines(filePath);
+            char[][] arrayOfStars = new char[rows.Length][];
+
+            for (int i = 0; i < rows.Length; i++)
             {
-                yield return new T[] { element }.Concat(combination);
+                arrayOfStars[i] = rows[i].ToCharArray();
             }
-            i++;
+
+            List<int>[] starCoords = new List<int>[arrayOfStars.Length];
+
+            for (int i = 0; i < arrayOfStars.Length; i++)
+            {
+                starCoords[i] = new List<int>();
+                for (int j = 0; j < arrayOfStars[i].Length; j++)
+                {
+                    starCoords[i].Add(j);
+                }
+            }
+        }
+
+        static IEnumerable<IEnumerable<T>> Combinations<T>(IEnumerable<T> elements, int k)
+        {
+            if (k == 0) yield return Enumerable.Empty<T>();
+
+            int i = 0;
+            foreach (T element in elements)
+            {
+                if (elements.Skip(i + 1).Any())
+                {
+                    foreach (var combination in Combinations(elements.Skip(i + 1), k - 1))
+                    {
+                        yield return new T[] { element }.Concat(combination);
+                    }
+                }
+                i++;
+            }
         }
     }
 }
-
